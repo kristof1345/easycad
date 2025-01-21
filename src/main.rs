215a48d66243d48964669345a6934bc814c59b2d
kristof1345 <kristofnemeth1345@gmail.com>
2025-01-21 07:42:37 +0100

    working

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
-use glutin::dpi::LogicalSize;
-use glutin::event::{Event, Event::WindowEvent};
-use glutin::event_loop::ControlFlow;
-use glutin::ContextBuilder;
-use glutin::GlRequest;
+use glutin::dpi::*;
+use glutin::GlContext;
 
 fn main() {
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let window = glutin::window::WindowBuilder::new()
+    let mut events_loop = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new()
         .with_title("EasyCAD")
-        .with_inner_size(LogicalSize::new(800.0, 600.0));
+        .with_dimensions(LogicalSize::new(1024.0, 768.0));
+    let context = glutin::ContextBuilder::new().with_vsync(true);
+    let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
 
-    let gl_context = ContextBuilder::new()
-        .with_gl(GlRequest::Latest)
-        .build_windowed(window, &event_loop)
-        .expect("Cannot create window context");
+    unsafe {
+        gl_window.make_current().unwrap();
+    }
 
-    let gl_context = unsafe {
-        gl_context
-            .make_current()
-            .expect("failed to make context current");
-    };
+    unsafe {
+        gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+        gl::ClearColor(0.0, 0.0, 0.0, 0.0);
+    }
 
-    gl::load_with(|symbol| gl_context.get_proc_address(symbol) as *const _);
-
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::LoopDestroyed => {}
-            Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+    let mut running = true;
+    while running {
+        events_loop.poll_events(|event| match event {
+            glutin::Event::WindowEvent { event, .. } => match event {
+                glutin::WindowEvent::CloseRequested => running = false,
+                glutin::WindowEvent::Resized(logical_size) => {
+                    let dpi_factor = gl_window.get_hidpi_factor();
+                    gl_window.resize(logical_size.to_physical(dpi_factor));
+                }
                 _ => (),
             },
             _ => (),
+        });
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
         }
-    })
+
+        gl_window.swap_buffers().unwrap();
+    }
 }
